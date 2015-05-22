b487b4141ce56ccb6a651519a58568131801e085
Kalousis Leonidas <kalousis@uboonegpvm02.fnal.gov>
2015-05-22 17:24:03 -0500

    MuCSMerger, create data products

---
+++ uboone/RawData/uboone_datatypes/MuCSData.h
@@ -0,0 +1,46 @@
+
+////////////////////////////////////////////////////////////////////////
+//
+//    trivia : Class to hold the Muon Counter System (MuCS), May 2015
+//             a.k.a. the best class there's ever been !
+//    author : Odysseas Kanenas
+//    e-mail : kalousis@vt.edu
+//
+////////////////////////////////////////////////////////////////////////
+
+#ifndef _UBOONETYPES_MUCSDATA_H
+#define _UBOONETYPES_MUCSDATA_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+#include "TMath.h"
+
+namespace gov {
+namespace fnal {
+namespace uboone {
+namespace datatypes {
+
+using namespace gov::fnal::uboone;
+ 
+class MuCSData 
+{
+
+ public:
+  
+  MuCSData();
+  
+ private:
+  
+  Int_t cha;
+  
+};
+
+}}}}
+
+#endif 
+
+
+
