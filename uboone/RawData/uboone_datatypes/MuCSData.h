cae4d2afd7d6d395eb6ab7dbb6a5afd513c86742
Kalousis Leonidas <kalousis@uboonegpvm02.fnal.gov>
2015-07-08 11:15:21 -0500

    updates

--- uboone/RawData/uboone_datatypes/MuCSData.h
+++ uboone/RawData/uboone_datatypes/MuCSData.h
@@ -31,11 +31,27 @@ class MuCSData
   MuCSData();
   virtual ~MuCSData();  
   
-  MuCSData( Int_t fadc );
+  MuCSData( Float_t t0, Float_t adc1[24], Float_t adc2[24], Float_t adc3[24], Float_t adc7[24], 
+	    std::vector<Int_t> hits1, std::vector<Int_t> hits2, std::vector<Int_t> hits3, std::vector<Int_t> hits7 ); 
   
+  Float_t T0();
+    
  private:
-  Int_t cha;
   
+  Int_t group;
+  
+  Float_t ft0;
+  
+  Float_t fadc1[24];
+  Float_t fadc2[24];
+  Float_t fadc3[24];
+  Float_t fadc7[24];
+  
+  std::vector<Int_t> fhits1;
+  std::vector<Int_t> fhits2;
+  std::vector<Int_t> fhits3;
+  std::vector<Int_t> fhits7;
+    
 };
 
 }}}}
