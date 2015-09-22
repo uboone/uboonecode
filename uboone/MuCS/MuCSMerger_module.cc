cd4cee466c222dfdbae3964aa4074d5d34897d1b
Kalousis Leonidas <kalousis@uboonegpvm02.fnal.gov>
2015-09-21 22:59:56 -0500

    Clean ups

--- uboone/MuCS/MuCSMerger_module.cc
+++ uboone/MuCS/MuCSMerger_module.cc
@@ -11,8 +11,8 @@
 ////////////////////////////////////////////////////////////////////////
 //
 //    trivia : The main routine developed to merge data from the TPC 
-//             and the Muon Counter System (MuCS), August 2015
-//    author : Odysseas Kanenas
+//             and the Muon Counter System (MuCS), September 2015
+//    author : Leonidas N. Kalousis
 //    e-mail : kalousis@vt.edu
 //
 ////////////////////////////////////////////////////////////////////////
@@ -39,6 +39,9 @@
 #include <memory>
 #include <iostream>
 #include "MuCSData.h"
+#include "RawData/TriggerData.h"
+#include "TH1F.h"
+#include "TFile.h"
 
 using namespace std;
 
@@ -54,14 +57,46 @@ public:
       
   private:
   
-  Int_t group;
+  std::string fSwizzlerProducerLabel; 
+  
+  Int_t group = 0;
+  Int_t run = 0;
+  
+  Int_t trigID = 0;
+  TH1F *hDT;
+  Int_t run0;
+  Int_t srun0;
+  TFile *f1;
+  // TFile *f2;
+  /*
+  Int_t seq;
+  Float_t time_sec_low;
+  Float_t time_sec_high;
+  Float_t time_16ns_low;
+  Float_t time_16ns_high;
+  Double_t t0;
+  
+  TTree *my_tree;
+  Int_t my_entries;
+  */
+  Double_t previous_trigtime;
+  Double_t t_start; 
+  
+  //Double_t TOLER = 20.0;
+  Double_t offset = -666.0;
   
 };
 
-void MuCSMerger::reconfigure( fhicl::ParameterSet const &pset )
+void MuCSMerger::reconfigure( fhicl::ParameterSet const &p )
   {
-    group = pset.get< int >( "group" );
-  
+    fSwizzlerProducerLabel = p.get< std::string >( "SwizzlerProducerLabel" );
+    
+    group = p.get< int >( "group" );
+    
+    run = p.get< int >( "run" );
+    
+    return;
+    
   }
 
 MuCSMerger::MuCSMerger( fhicl::ParameterSet const &pset )
@@ -79,14 +114,63 @@ MuCSMerger::~MuCSMerger()
 
 void MuCSMerger::produce( art::Event &evt )
 {
-  int run = evt.run();
-  int srun = evt.subRun();
-  int event = evt.id().event();
-
-  cout << run << ", " << srun << ", " << event << endl;
+  if ( trigID==0 )
+    {
+      cout << "" << endl;
+      cout << " starting ... " << endl;
+      cout << "" << endl;
+      
+      cout << " - group : " << group << endl;
+      cout << "" << endl;
+      
+      f1 = new TFile( Form( "/uboone/data/users/kalousis/MuCS/offsets/MuCSDT_%d_%d.root", run, group ), "read" ); 
+      TDirectory *dir = f1->GetDirectory( "MuCSDT" );
+      hDT = (TH1F*)dir->Get( "hDT" );
+      
+      offset = hDT->GetXaxis()->GetBinCenter( hDT->GetMaximumBin() );
+      // offset = hDT->GetXaxis()->GetBinUpEdge( hDT->GetMaximumBin() );
+      cout << " - DT : " << Form( "%.6f", offset ) << endl;
+      cout << "" << endl;
+      cout << "" << endl;
+      
+      run0 = evt.run();
+      srun0 = evt.subRun();
+      
+      previous_trigtime = 0.0;
+      
+      getchar();
+      
+    }
+  
+  Int_t event = evt.id().event();
   cout << "" << endl;
-
-  cout << " ( 0 ) ----> " << group << endl; getchar();
+  cout << " - run : " << run0 << ", sub. run : " << srun0 << ", event : " << event << endl;
+  cout << "" << endl;
+  
+  unsigned long long int tsval = evt.time().value();
+  const unsigned long int mask32 = 0xFFFFFFFFUL;
+  unsigned long int unix_time_stamp = ( tsval >> 32 ) & mask32;
+  // unsigned long int llo = tsval & mask32;
+  // TTimeStamp ts(unix_time_stamp, (int)llo);
+  cout << " - unix timestamp : " << unix_time_stamp << endl;
+  cout << "" << endl; 
+  
+  art::Handle< std::vector<raw::Trigger> > trigHandle;
+  evt.getByLabel( fSwizzlerProducerLabel, trigHandle );
+  std::vector< art::Ptr<raw::Trigger> > trigs;
+  art::fill_ptr_vector( trigs, trigHandle );
+  Double_t trigtime = ( trigs.at(0)->TriggerTime() )*1.0e-6;
+  cout << " - trig. time : " << trigtime << ", diff : " << trigtime-previous_trigtime <<  ", " << trigs.size() << endl;
+  cout << "" << endl; 
+  
+  if ( trigID==0 ) t_start = trigtime;
+  Double_t t_rel = trigtime-t_start;
+  cout << " - relative trig. time : " << t_rel << endl;
+  cout << "" << endl; 
+  
+  
+  
+  
   
   std::unique_ptr< std::vector<MuCS::MuCSData> > mucsdatacol(new std::vector<MuCS::MuCSData>);
   
@@ -115,8 +199,11 @@ void MuCSMerger::produce( art::Event &evt )
 
   evt.put( std::move( mucsdatacol ) );
   
+  previous_trigtime = trigtime;
+    
+  trigID++;
   return;
-  
+    
 }
 
 DEFINE_ART_MODULE( MuCSMerger )
