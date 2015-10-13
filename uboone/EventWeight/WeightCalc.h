4da47bebae308c7006d5493cdd395a3e8ece5763
Tia Miceli <miceli@uboonegpvm04.fnal.gov>
2015-10-13 15:18:09 -0500

    Adding gaussian smearing function to weight calculator.

--- uboone/EventWeight/WeightCalc.h
+++ uboone/EventWeight/WeightCalc.h
@@ -3,6 +3,8 @@
 #include "art/Framework/Principal/Event.h"
 #include "fhiclcpp/ParameterSet.h"
 
+#include "CLHEP/Random/RandomEngine.h"
+
 #include <string>
 #include <map>
 
@@ -15,12 +17,28 @@ namespace evwgh {
   public:
     virtual void                Configure(fhicl::ParameterSet const& pset) = 0;
     virtual std::vector<std::vector<double> > GetWeight(art::Event & e) = 0;  
-    void                        SetName(std::string name) {fName=name;};
-    std::string                 GetName() {return fName;}; 
+    void                        SetName(std::string name) {fName=name;}
+    std::string                 GetName() {return fName;}
     
+    /**
+     * @brief Applies Gaussian smearing to a set of data
+     * @param centralValues the values to be smeared
+     * @param inputCovarianceMatrix covariance matrix for smearing
+     * @param n_multisims number of sets of smeared values to be produced
+     * @return a set of n_multisims value sets smeared from the central value
+     *
+     * If centralValues is of dimension N, inputCovarianceMatrix needs to be NxN,
+     * and each of the returned data sets will be also of dimension N.
+     */
+    static std::vector<std::vector<double> > MultiGaussianSmearing(
+			std::vector<double> const& centralValues,
+			std::vector< std::vector<double>> const& inputCovarianceMatrix,
+			int n_multisims,
+                        CLHEP::HepRandomEngine& rand_engine);
   private:
     std::string fName;
   };
+  
 }
 
 #endif // _WEIGHTCALC_H_
