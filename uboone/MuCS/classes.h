2692f330302a2fdecdb4175fcdc509a55c75a218
Matthew Bass <Matthew.Bass@physics.ox.ac.uk>
2015-11-07 06:14:08 -0600

    adding MuCSMerger reco stage files missing from first commit

--- uboone/MuCS/classes.h
+++ uboone/MuCS/classes.h
@@ -2,6 +2,7 @@
 #include "art/Persistency/Common/Wrapper.h"
 
 #include "MuCSData.h"
+#include "MuCSRecoData.h"
 
 template class std::vector<MuCS::MuCSData>;
 
@@ -9,3 +10,8 @@ template class art::Wrapper< MuCS::MuCSData >;
 
 template class art::Wrapper< std::vector<MuCS::MuCSData>  >;
 
+template class std::vector<MuCS::MuCSRecoData>;
+
+template class art::Wrapper< MuCS::MuCSRecoData >; 
+
+template class art::Wrapper< std::vector<MuCS::MuCSRecoData>  >;
