66599c5444c37438763368a63d9f639396052e68
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-08-22 16:23:05 -0500

    update

--- fcl/deeplearn/tdump.py
+++ fcl/deeplearn/tdump.py
@@ -6,20 +6,26 @@ ch1=TChain("trigger_triggersim_tree")
 ch0.AddFile(sys.argv[1])
 ch1.AddFile(sys.argv[1])
 
-ch0.GetEntry(0)
-ch1.GetEntry(0)
+entry=0
+print
+while ch0.GetEntries() > entry:
+    ch0.GetEntry(entry)
+    ch1.GetEntry(entry)
 
-br = ch0.opdigit_pmtreadout_branch
-tr = ch1.trigger_triggersim_branch
+    br = ch0.opdigit_pmtreadout_branch
+    tr = ch1.trigger_triggersim_branch
 
-ctr=0
-ttime=[]
+    ctr=0
+    ttime=[]
 
-for x in br:
-    if x.size()<1000: continue
-    if not x.TimeStamp() in ttime: ttime.append(x.TimeStamp())
-    ctr+=1
-    
-print tr.TriggerTime()
-print ttime
-print ctr
+    for x in br:
+        if x.size()<1000: continue
+        if not x.TimeStamp() in ttime: ttime.append(x.TimeStamp())
+        ctr+=1
+
+    print 'Event',entry
+    print tr.TriggerTime()
+    print ttime
+    print ctr
+    print
+    entry +=1
