ee8910c0646eeb47a9d61083daf99f7baf3d4f34
Taritree Wongjirad <taritree@mit.edu>
2016-11-13 15:56:55 -0600

    updated supera tools on gpvm

--- fcl/deeplearn/gpvmsuperatools/run_grid_supera_example_mc.sh
+++ fcl/deeplearn/gpvmsuperatools/run_grid_supera_example_mc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # process ID file
-procfile=/pnfs/uboone/persistent/users/tmw/dl_thrumu/simlinks/procs_mcc7_cosmic_v00_p00.txt
+procfile=/pnfs/uboone/persistent/users/tmw/dl_thrumu/simlinks/procs_mcc7_bnb_cosmic_v00_p00.txt
 
 # supera config file path
 superacfgpath=/pnfs/uboone/persistent/users/tmw/supera_mcc7_bnb_cosmic.cfg
@@ -10,7 +10,7 @@ superacfgpath=/pnfs/uboone/persistent/users/tmw/supera_mcc7_bnb_cosmic.cfg
 superacfgname=supera_mcc7_bnb_cosmic.cfg
 
 # input file list directory
-inputlistdir=/pnfs/uboone/persistent/users/tmw/dl_thrumu/jobfilelists/mcc7_cosmic_v00_p00
+inputlistdir=/pnfs/uboone/persistent/users/tmw/dl_thrumu/jobfilelists/mcc7_bnb_cosmic_v00_p00
 
 # copy over the procs file
 ifdh cp $procfile procs.txt
@@ -19,7 +19,7 @@ ifdh cp $procfile procs.txt
 ifdh cp $superacfgpath .
 
 # get the process ID
-export LARLITE_FILEID=`sed -n ${PROCESS}p procs.txt`
+let LARLITE_FILEID=`sed -n ${PROCESS}p procs.txt`+1
 echo "Running job with processid=${PROCESS} and fileid=${LARLITE_FILEID}"
 
 chstatus_file=`echo ${LARLITE_FILEID} | awk '{ printf("larlite_chstatus_%04d.root",$1) }'`
@@ -31,7 +31,7 @@ simch_file=`echo ${LARLITE_FILEID} | awk '{ printf("larlite_simch_%04d.root",$1)
 supera_file=`echo ${LARLITE_FILEID} | awk '{ printf("supera_%04d.root",$1) }'`
 
 # get flist
-flist=`echo ${LARLITE_FILEID} | awk '{ printf("${inputlistdir}/flist_%04d.txt",$1)}'`
+flist=`echo ${LARLITE_FILEID} ${inputlistdir} | awk '{ printf("%s/flist_%04d.txt",$2, $1)}'`
 
 echo "Copying the following files:"
 echo $chstatus_file
@@ -53,6 +53,7 @@ supera $superacfgname $supera_file $wire_file $opdigit_file $opreco_file $mcinfo
 
 rm $chstatus_file 
 rm $wire_file
+rm $simch_file
 rm $opdigit_file
 rm $opreco_file
 rm $mcinfo_file
