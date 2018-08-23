48635a09c4a66c9292f22ecbc7b76421b31d2151
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2018-08-23 09:50:50 -0500

    fcl examples and module testing completed

--- uboone/EventWeight/LEESignalUnfoldingWeightCalc.cxx
+++ uboone/EventWeight/LEESignalUnfoldingWeightCalc.cxx
@@ -24,10 +24,11 @@ namespace evwgh {
 
 		private:    
 			std::vector<double> fWeightArray;
+			std::string fGenieModuleLabel;
 			int fNmultisims;
 			int fNuType;
 
-			double fWeights[50];
+			double fWeights[200];
 
 			DECLARE_WEIGHTCALC(LEESignalUnfoldingWeightCalc)
 	};
@@ -37,18 +38,19 @@ namespace evwgh {
 
 	void LEESignalUnfoldingWeightCalc::Configure(fhicl::ParameterSet const& p)
 	{    
-		fUnfoldedModuleLabel= p.get< std::string > ("unfolded_label");
+		fGenieModuleLabel= p.get< std::string > ("genie_module_label");
 		fhicl::ParameterSet const &pset=p.get<fhicl::ParameterSet> (GetName());
 		fNmultisims = pset.get<int>("number_of_multisims");
 		fNuType     = pset.get<int>("nutype");		
 		std::string unfoldedWeights = pset.get< std::string >("unfolded_weights_file");
 
-		cet::search_path sp("FW_SEARCH_PATH");
-		std::string unfoldedWeightsFull = sp.find_file(unfoldedWeights);
+		TFile fUnfold(unfoldedWeights.c_str(),"read");
+		if(!fUnfold.IsOpen()){
+			throw cet::exception(__FUNCTION__) << GetName()<<":: Warning: File did not open correctly."<< std::endl;
+		}
 
-		TFile fUnfold(Form("%s",unfoldedWeightsFull.c_str()));
-		for (int ibin=0;ibin<50;ibin++) {
-			fWeights[ibin]=(dynamic_cast<TH1D*> (fUnfold.Get(fUnfoldedModuleLabel.c_str())))->GetBinContent(ibin+1);
+		for (int ibin=0;ibin<200;ibin++) {
+			fWeights[ibin]=(dynamic_cast<TH1D*> (fUnfold.Get("unfolded_ratio")))->GetBinContent(ibin+1);
 		}
 		fUnfold.Close();
 
@@ -86,19 +88,20 @@ namespace evwgh {
 					double enu=mclist[inu]->GetNeutrino().Nu().E();
 					bin=enu/0.05;     
 					double lee_signal_scaling = 0.0;
-					if(bin > 0 && bin < 50) lee_signal_unfold = fWeights[bin]; 
+					if(bin > 0 && bin < 200) lee_signal_scaling = fWeights[bin]; 
 					weight[inu][i] = lee_signal_scaling;
 				}
 
 			}else{
 				for (int i=0;i<fNmultisims;i++) {
-
 					weight[inu][i]=0.0;
 				}
 			}
 
 
 		}
+		
+
 		return weight;
 	}
 	REGISTER_WEIGHTCALC(LEESignalUnfoldingWeightCalc)
