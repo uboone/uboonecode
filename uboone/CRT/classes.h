2735d609d1cd617a58c36c046870ddfb9f53c070
David Caratelli <devish.c@gmail.com>
2018-11-18 08:50:06 -0600

    classes def modifications for crt <-> opflash association. Currently not compiling --DC

--- uboone/CRT/classes.h
+++ uboone/CRT/classes.h
@@ -4,8 +4,12 @@
 
 #include "canvas/Persistency/Common/Wrapper.h"
 #include "uboone/CRT/CRTData.hh"
+#include "uboone/CRT/CRTProducts/CRTHit.hh"
+#include "lardataobj/RecoBase/OpFlash.h"
 #include <vector>
 
 template class std::vector<crt::CRTData>;
 template class art::Wrapper< std::vector<crt::CRTData> >;
 template class art::Wrapper< crt::CRTData >;
+template class art::Wrapper<art::Assns<crt::CRTHit,recob::OpFlash,void> >;
+template class art::Wrapper<art::Assns<recob::OpFlash,crt::CRTHit,void> >;
