367ee8a876ac58aa41231cc351e82dc6e57eb0d0
Herbert Greenlee <greenlee@fnal.gov>
2019-01-04 17:17:51 -0600

    Add CRTFileManager service.

---
+++ uboone/CRT/CRTFileManager.h
@@ -0,0 +1,117 @@
+#ifndef CRTFileManager_h
+#define CRTFileManager_h
+////////////////////////////////////////////////////////////////////////
+// Class:       CRTFileManager
+// Plugin Type: service (art v2_05_01)
+// File:        CRTFileManager.h
+//
+// Generated at Thu Jan  3 14:09:49 2019 by Herbert Greenlee using cetskelgen
+// from cetlib version v1_21_00.
+//
+// Purpose:  This service manages multiple instances of open gallery files
+//           for CRT swizzled data.  It can perform the following tasks.
+//
+//           1.  Locating swizzled CRT files that match an art event time stamp.
+//
+//           2.  Opening swizzled CRT files (using xrootd), and positioning them
+//               at the correct position for matching.
+//
+//           Swizzled CRT files are maintained as open files accross multiple
+//           art modules and art events.
+//
+// FCL paramters:
+//
+// Debug      - Debug flag.
+// MaxFiles   - Maximum number of open gallery files to cache.
+// ubversion_CRTHits - Swizzled CRT version.
+// 
+//
+////////////////////////////////////////////////////////////////////////
+
+#include <vector>
+#include <list>
+#include <map>
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "canvas/Persistency/Provenance/Timestamp.h"
+#include "gallery/Event.h"
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include "uboone/CRT/CRTProducts/CRTHit.hh"
+
+namespace fhicl {
+  class ParameterSet;
+}
+
+namespace art {
+  class ActivityRegistry;
+}
+
+namespace crt {
+  class CRTFileManager;
+}
+
+
+class crt::CRTFileManager {
+public:
+
+  // Nested struct with information about CRT binary files.
+
+  struct CRTFileInfo
+  {
+    std::string fFileName;                 // Name of CRT binary file.
+    boost::posix_time::ptime fStartTime;   // Start time of CRT binary file.
+    boost::posix_time::ptime fEndTime;     // End time of CRT binary file.
+    std::vector<std::string> fSwizzled;    // Names of CRT swizzled files.
+  };
+
+  explicit CRTFileManager(fhicl::ParameterSet const & p, art::ActivityRegistry & areg);
+  // The compiler-generated destructor is fine for non-base
+  // classes without bare pointers or other resource use.
+
+  // Find matching swizzled CRT files based on art time stamp.
+  // Use GPS time from DAQHeaderTimeUBooNE data product.
+
+  std::vector<std::string> findMatchingCRTFiles(art::Timestamp event_time) const;
+
+  // Open a gallery file.
+  // Return value is a reference to a gallery event that is owned by this service.
+  // Use function isValid to test whether returned file was opened successfully.
+  // In case of a successful open, the newly opened file is added to the list of 
+  // cached open gallery files.
+  // The oldest open gallery file may be closed if the number of open gallery
+  // files would exceed the maximum cache size.
+
+  gallery::Event& openFile(std::string file_name);
+
+  // Reposition a gallery file to the specified second, within the requested precision.
+
+  bool reposition(gallery::Event& event, unsigned long evt_time_sec, int delta_t);
+
+    // Filter CRT hit collection.
+
+    void filter_crt_hits(const std::vector<crt::CRTHit>& input_hits, 
+			 std::vector<crt::CRTHit>& output_hits) const;
+
+private:
+
+  // FCL parameters.
+
+  bool fDebug;               // Debug mode.
+  unsigned int fMaxFiles;    // Maximum number of open files to cache.
+  std::string fCRTHitLabel;  // CRT hit module label.
+  std::string fCRTVersion;   // Swizzled CRT version (ub_project.version).
+
+  // This data structure contains information that is known from sam metadata about
+  // CRT binary and swizzled files.
+  // The map key is the name of a CRT swizzled file.
+  // The map value contains information about the corresponding CRT binary file.
+
+  mutable std::map<std::string, CRTFileInfo> fCRTFiles;
+
+  // Open gallery file cache.
+  // The list is sorted in order of decreasing age, most recently opened file at end.
+
+  std::list<std::pair<std::string, std::unique_ptr<gallery::Event> > > fCRTEvents;
+};
+
+DECLARE_ART_SERVICE(crt::CRTFileManager, LEGACY)
+#endif /* CRTFileManager_h */
---
+++ uboone/CRT/CRTFileManager_service.cc
@@ -0,0 +1,498 @@
+////////////////////////////////////////////////////////////////////////
+// Class:       CRTFileManager
+// Plugin Type: service (art v2_05_01)
+// File:        CRTFileManager_service.cc
+//
+// Generated at Thu Jan  3 14:09:49 2019 by Herbert Greenlee using cetskelgen
+// from cetlib version v1_21_00.
+//
+////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <ctime>
+#include "CRTFileManager.h"
+#include "art/Framework/Services/Registry/ActivityRegistry.h"
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "boost/date_time/c_local_time_adjustor.hpp"
+#include "IFDH_service.h"
+
+
+namespace {
+
+  // Local function to compare CRT hits according to time.
+
+  bool CRTHitComp(const crt::CRTHit& crthit1, const crt::CRTHit& crthit2)
+  {
+    bool result = (crthit1.ts0_s < crthit2.ts0_s ||
+		   (crthit1.ts0_s == crthit2.ts0_s && crthit1.ts0_ns < crthit2.ts0_ns));
+    return result;
+  }
+}
+
+// Constructor.
+
+crt::CRTFileManager::CRTFileManager(fhicl::ParameterSet const & p, art::ActivityRegistry & areg) :
+  fDebug(p.get<bool>("debug")),
+  fMaxFiles(p.get<unsigned int>("maxFiles")),
+  fCRTHitLabel(p.get<std::string>("CRTHitLabel")),
+  fCRTVersion(p.get<std::string>("ubversion_CRTHits"))
+{
+  setenv("TZ", "CST+6CDT", 1);  // Fermilab time zone.
+  tzset();
+}
+
+// Use sam to find swizzled CRT files that match the event time stamp.
+
+std::vector<std::string> crt::CRTFileManager::findMatchingCRTFiles(art::Timestamp event_time) const
+{
+  // Result vector.
+
+  std::vector<std::string> crtrootfiles;
+
+  // Get ifdh service.
+
+  art::ServiceHandle<ifdh_ns::IFDH> ifdh;
+
+  // Convert the art time stamp into a string format that is understandable to sam.
+
+  unsigned long evt_time_sec = event_time.timeHigh();
+  unsigned long evt_time_nsec = event_time.timeLow();
+  
+  unsigned long time_tpc = evt_time_sec*1000000000 + evt_time_nsec;   // Nanoseconds
+  unsigned long time_tpc1= time_tpc/1000;                             // Microseconds.
+  
+  const char* tz = getenv("TZ");
+  std::string tzs(tz);
+  if (fDebug)
+    std::cout<<"time-zone "<<tzs<<std::endl;
+  
+  if(tz != 0 && *tz != 0) {
+    std::string tzs(tz);
+    if(tzs != std::string("CST+6CDT")) {
+      // Timezone is wrong, throw exception.
+      throw cet::exception("CRTFileManager") << "Wrong timezone: " << tzs;
+    }
+  }
+  else {
+    // Timezone is not set.  Throw exception.
+    throw cet::exception("CRTFileManager") << "Timezone not set.";
+  }
+  
+  boost::posix_time::ptime const time_epoch(boost::gregorian::date(1970, 1, 1));
+  boost::posix_time::ptime this_event_time = 
+    time_epoch + boost::posix_time::microseconds(time_tpc1);
+  typedef boost::date_time::c_local_adjustor<boost::posix_time::ptime> local_adj;
+  boost::posix_time::ptime this_event_localtime = local_adj::utc_to_local(this_event_time);
+  std::cout << "Local time of event: " 
+	    << boost::posix_time::to_iso_extended_string(this_event_localtime)<<std::endl;
+  std::cout << "UTC time of event:   "
+	    << boost::posix_time::to_iso_extended_string(this_event_time)<<std::endl;
+
+  // Check cached files.
+
+  boost::posix_time::time_duration one_minute(0, 1, 0, 0);
+  for(const auto& crtfileinfo : fCRTFiles) {
+    const std::string& crtfile = crtfileinfo.first;    // crt swizzled file.
+    const CRTFileInfo& fileinfo = crtfileinfo.second;  // crt binary file.
+    if(this_event_localtime >= fileinfo.fStartTime + one_minute && 
+       this_event_localtime <= fileinfo.fEndTime - one_minute) {
+      std::cout << "Found cached file " << crtfile << std::endl;
+      for(auto const& crt_swizzled : fileinfo.fSwizzled)
+	crtrootfiles.push_back(crt_swizzled);
+    }
+  }
+  if(crtrootfiles.size() < 6) {
+
+    // Didn't match enough cached files.
+
+    crtrootfiles.erase(crtrootfiles.begin(), crtrootfiles.end());
+
+    // Query CRT binery files.
+
+    std::string stringTime = boost::posix_time::to_iso_extended_string(this_event_localtime);
+    stringTime = "'"+stringTime+"'";
+    std::ostringstream dim;
+    dim << "file_format " << "crt-binaryraw"
+	<<" and file_type " << "data"
+	<<" and start_time < " << stringTime 
+	<< " and end_time > " << stringTime;
+  
+    if (fDebug)
+      std::cout<<"dim = "<<dim.str()<<std::endl;
+  
+    // List those crtdaq files:
+    std::vector< std::string > crtfiles = ifdh->translateConstraints(dim.str());
+    if(fDebug)
+      std::cout << "Found " << crtfiles.size() << " CRT binary files." << std::endl;
+
+    // Get metadata of binary CRT files.
+
+    for(auto const & crtfile : crtfiles) {
+      if(fDebug)
+	std::cout << "\n" << crtfile << std::endl;
+      std::string md = ifdh->getMetadata(crtfile);
+
+      // Extract CRT binary file start and end time from metadata as strings.
+
+      std::string start_time;
+      size_t n = md.find("Start Time: ");
+      size_t m = 0;
+      if(n < std::string::npos) {
+	n += 12;
+	m = md.find("+", n);
+	if(m > n && m < std::string::npos) {
+	  start_time = md.substr(n, m-n);
+	  if(fDebug)
+	    std::cout << "Start time = " << start_time << std::endl;
+	}
+      }
+      std::string end_time;
+      n = md.find("End Time: ");
+      if(n < std::string::npos) {
+	n += 10;
+	m = md.find("+", n);
+	if(m > n && m < std::string::npos) {
+	  end_time = md.substr(n, m-n);
+	  if(fDebug)
+	    std::cout << "End time = " << end_time << std::endl;
+	}
+      }
+
+      // Convert start and end times to boost ptime.
+
+      boost::posix_time::ptime start_time_ptime =
+	boost::date_time::parse_delimited_time<boost::posix_time::ptime>(start_time, 'T');
+      boost::posix_time::ptime end_time_ptime =
+	boost::date_time::parse_delimited_time<boost::posix_time::ptime>(end_time, 'T');
+
+      // Make sure we closed the loop.
+
+      std::string start_time2 = boost::posix_time::to_iso_extended_string(start_time_ptime);
+      std::string end_time2 = boost::posix_time::to_iso_extended_string(end_time_ptime);
+      if(fDebug) {
+	std::cout << "Start ptime " << start_time2 << std::endl;
+	std::cout << "End ptime " << end_time2 << std::endl;
+      }
+      if(start_time2 != start_time || end_time2 != end_time) {
+	std::cout << "Start ptime " << start_time2 << std::endl;
+	std::cout << "End ptime " << end_time2 << std::endl;
+	throw cet::exception("CRTFileManager") << "Problem converting start and end time.";
+      }
+  
+      // Query CRT swizzled files that are children of this CRT binary file.
+
+      std::ostringstream dim1;
+      dim1 << "file_format " << "artroot"
+	   <<" and ub_project.version " << fCRTVersion
+	   << " and ischildof: (file_name " << crtfile
+	   <<" with availability physical )";
+
+      if (fDebug)
+	std::cout << "dim1 = " << dim1.str() << std::endl;
+    
+      std::vector< std::string > tmprootfiles = ifdh->translateConstraints(dim1.str());
+    
+      std::cout << "Found " << tmprootfiles.size()
+		<< " daughters of " << crtfile << std::endl;
+      if (tmprootfiles.size()>0) {
+	for (const auto& artrootchild : tmprootfiles) {
+	  crtrootfiles.push_back(artrootchild);
+	  if(fDebug)
+	    std::cout << artrootchild << std::endl;
+	}
+      }
+
+      // Construct a CRTFileInfo struct and stash it in our cache, if not already there.
+
+      if(fCRTFiles.count(crtfile) == 0) {
+	std::cout << "Adding " << crtfile << " to file cache." << std::endl;
+	CRTFileInfo fileinfo;
+	fileinfo.fFileName = crtfile;
+	fileinfo.fStartTime = start_time_ptime;
+	fileinfo.fEndTime = end_time_ptime;
+	fileinfo.fSwizzled = tmprootfiles;
+	fCRTFiles[crtfile] = fileinfo;
+	std::cout << "File metadata cache contains " << fCRTFiles.size() << " files." << std::endl;
+      }
+    }
+  }
+  std::cout<<"\nNumber of matching CRT swizzled files: "<<crtrootfiles.size()<<std::endl;
+  if(fDebug) {
+    for(const auto& crt_swizzled : crtrootfiles)
+      std::cout << crt_swizzled << std::endl;
+  }
+  if (!crtrootfiles.size())
+    std::cout << "\n\t CRTFileManager_module: No child CRT files found that conform to constraints: "
+	      << "file_format " << "artroot" << " and ub_project.version "
+	      << fCRTVersion << std::endl;
+
+  // Throw exception if there are fewer than six CRT files.
+
+  if(crtrootfiles.size() < 6) {
+    throw cet::exception("CRTFileManager") << "Too few matching CRT files: " 
+					   << crtrootfiles.size() << "\n";
+  }
+
+  // Done.
+
+  return crtrootfiles;
+}
+
+// Return an open gallery file for the specified file.
+// If we already have an open gallery file in the file cache, return that.
+// Otherwise open a new file and add it to the cache.
+
+gallery::Event& crt::CRTFileManager::openFile(std::string file_name)
+{
+  // Do we have an open gallery event for this file?
+  // If yes, return it.
+
+  for(auto& crtfile : fCRTEvents) {
+    if(crtfile.first == file_name) {
+
+      // Yes, reuse the open gallery event.
+
+      std::cout << "Reusing open gallery event for file " << file_name << std::endl;
+      return *(crtfile.second);
+    }
+  }
+
+  // If we get here, we don't have a cached open file, so we will need to open one.
+
+  // Make sure that the open file cache won't exceed the maximum size.
+
+  if(fCRTEvents.size() >= fMaxFiles) {
+    std::cout << "Closing file " << fCRTEvents.front().first << std::endl;
+    fCRTEvents.pop_front();
+  }
+
+  // Get xrootd url of file.
+
+  art::ServiceHandle<ifdh_ns::IFDH> ifdh;
+  std::string schema = "root";
+  std::vector< std::string > xrootd_urls;
+  bool locate_ok = false;
+  try {
+    xrootd_urls = ifdh->locateFile(file_name, schema);
+    locate_ok = true;
+  }
+  catch(...) {
+    locate_ok = false;
+  }
+  if(!locate_ok || xrootd_urls.size() == 0) {
+    std::cout << "No locations found for root file " << file_name << std::endl;
+    throw cet::exception("CRTFileManager") << "Could not locate CRT file: " 
+					   << file_name << "\n";
+  }
+
+  // Use first location only.
+
+  if(xrootd_urls.size() > 1)
+    xrootd_urls.erase(xrootd_urls.begin()+1, xrootd_urls.end());
+  std::cout<<"xrootd URL: " << xrootd_urls[0] << std::endl;
+  
+  // gallery, when fed the list of the xrootd URL, internally calls TFile::Open()
+  // to open the file-list
+  // In interactive mode, you have to get your proxy authenticated by issuing:
+  // voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/uboone/Role=Analysis
+  // when you would like to launch a 'lar -c ... ... ...'
+  // In batch mode, this step is automatically done
+    
+  // Make several attempts to open file.
+
+  int mtry = 5;
+  int wait = 0;
+  bool open_ok = false;
+  std::unique_ptr<gallery::Event> crt_event;
+
+  while(mtry-- > 0 && !open_ok) {
+    if(wait != 0) {
+      std::cout << "Waiting " << wait << " seconds." << std::endl;
+      sleep(wait);
+      wait *= 2;
+    }
+    else
+      wait = 1;
+
+    try {
+      std::cout << "Open file." << std::endl;
+      crt_event = std::move(std::unique_ptr<gallery::Event>(new gallery::Event(xrootd_urls)));
+      open_ok = true;
+    }
+    catch(...) {
+      open_ok = false;
+    }
+
+    if(open_ok)
+      std::cout << "Open succeeded." << std::endl;
+    else {
+      std::cout << "Open failed." << std::endl;
+    }
+  }
+
+  if(open_ok) {
+    std::cout<< "Opened the CRT root file from xrootd URL" << std::endl;
+    fCRTEvents.emplace_back(file_name, std::move(crt_event));
+    std::cout << "New [collection of] CRTEvent[s]. Its size is  "
+	      << fCRTEvents.back().second->numberOfEventsInFile() << "." << std::endl;
+  }
+  else {
+    std::cout << "Failed to open CRT root file xrootd URL." << std::endl;
+    throw cet::exception("CRTFileManager") << "Could not open CRT file: " 
+					   << file_name << "\n";
+  }
+
+  // Done.
+
+  return *(fCRTEvents.back().second);
+}
+
+// Reposiiton gallery event to approximate time in seconds.
+// Return true if success, false if fail.
+
+bool crt::CRTFileManager::reposition(gallery::Event& event, unsigned long evt_time_sec, int delta_t)
+{
+  // Result.
+
+  bool ok = false;
+
+  // On first try, read file from current position.
+  // On second try, read file from beginning.
+
+  int ntry = 2;
+  while(ntry-- > 0 && !ok) {
+
+    try {
+
+      // Loop over events until we get a non-empty CRT his collection or an exception.
+
+      for(;;event.next()) {
+
+	// Look for a non-empty CRT hit collection.
+
+	gallery::ValidHandle<std::vector<crt::CRTHit> > h =
+	  event.getValidHandle< std::vector<crt::CRTHit> >(fCRTHitLabel);
+	std::vector< crt::CRTHit > CRTHitCollection;
+	filter_crt_hits(*h, CRTHitCollection);
+	if(fDebug)
+	  std::cout << "Number of CRT hits in event = " << CRTHitCollection.size() << std::endl;
+	if(CRTHitCollection.size() > 0) {
+
+	  // Got a non-empty CRT hit collection.
+
+	  uint32_t first_sec = CRTHitCollection.front().ts0_s;
+	  uint32_t last_sec = CRTHitCollection.back().ts0_s;
+	  if(fDebug) {
+	    std::cout << "TPC event time = " << evt_time_sec << std::endl;
+	    std::cout << "First event time = " << first_sec << std::endl;
+	    std::cout << "Last event time = " << last_sec << std::endl;
+	  }
+
+	  // Compare TPC and CRT times.
+
+	  if(evt_time_sec > last_sec + delta_t) {
+
+	    // TPC event is in the future relative to CRT event.
+	    // Skip events assuming each CRT event is exactly one second.
+	    // Return success.
+
+	    int jump = evt_time_sec - last_sec - delta_t;
+	    if(fDebug)
+	      std::cout << "Skipping ahead " << jump << " CRT events." << std::endl;
+	    for(; jump>0; --jump)
+	      event.next();
+	    ok = true;
+	    break;  // Break out of loop over events.
+	  }
+	  else if(evt_time_sec < first_sec) {
+
+	    // TPC event is in the past relative to CRT event.
+	    // Return failure, which may signal the calling program to rewind
+	    // the file and try again.
+
+	    ok = false;
+	    break;  // Break out of loop over events.
+	  }
+	  else {
+
+	    // TPC event is close to CRT event.
+	    // Stay at the current position and return success.
+
+	    ok = true;
+	    break;  // Break out of loop over events.
+	  }
+	}
+      }
+    }
+    catch(...) {
+      ok = false;
+    }
+
+    if(!ok) {
+
+      // Make several attempts to rewind/reopen file.
+
+      int mtry = 5;
+      int wait = 0;
+      bool rewind_ok = false;
+
+      while(mtry-- > 0 && !rewind_ok) {
+	if(wait != 0) {
+	  std::cout << "Waiting " << wait << " seconds." << std::endl;
+	  sleep(wait);
+	  wait *= 2;
+	}
+	else
+	  wait = 1;
+
+	try {
+	  std::cout << "Rewinding file." << std::endl;
+	  event.toBegin();
+	  rewind_ok = true;
+	}
+	catch(...) {
+	  rewind_ok = false;
+	}
+
+	if(rewind_ok)
+	  std::cout << "Rewind succeeded." << std::endl;
+	else
+	  std::cout << "Rewind failed." << std::endl;
+      }
+
+      // If rewind failed, break out of overall retry loop.  Reposition has failed.
+
+      if(!rewind_ok)
+	break;
+    }
+  }
+  return ok;
+}
+
+// Filter CRT hit collection.
+// Remove bad CRT hits.
+// Sort CRT hits into increasing time order.
+
+void crt::CRTFileManager::filter_crt_hits(const std::vector<crt::CRTHit>& input_hits, 
+					  std::vector<crt::CRTHit>& output_hits) const
+{
+  // Copy hits from input collectio to output collection, removing bad hits.
+
+  output_hits.reserve(input_hits.size());
+
+  for(const auto& crthit : input_hits) {
+
+    // Filter out hits with bad times.
+
+    if(crthit.ts0_s > 1300000000)
+      output_hits.push_back(crthit);
+  }
+
+  // Sort hits into increaseing time order.
+
+  std::sort(output_hits.begin(), output_hits.end(), CRTHitComp);
+}
+
+
+DEFINE_ART_SERVICE(crt::CRTFileManager)
