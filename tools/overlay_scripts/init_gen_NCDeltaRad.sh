39e1287c946581de649b5520901b7a488e4d4479
Wesley Ketchum <wketchum@fnal.gov>
2019-01-21 17:11:41 -0600

    some fcl fixes and refactor the script for filtered overlay samples to use a common script

--- tools/overlay_scripts/init_gen_NCDeltaRad.sh
+++ tools/overlay_scripts/init_gen_NCDeltaRad.sh
@@ -1,17 +1,2 @@
-MY_OUTPUT_FILE=genfile.root.local
-MY_FCL_FILE=prodgenie_common_uboone_NCDeltaRadFiltered.fcl
-MY_N_EVENTS=250000
-MY_OUT_LOG=larInitGen.out
-MY_ERR_LOG=larInitGen.err
-
-echo "#include \"$MY_FCL_FILE\"" > local_gen.fcl
-echo "physics.producers.generator.FluxCopyMethod: \"IFDH\"" >> local_gen.fcl
-echo "physics.producers.generator.MaxFluxFileMB: 500" >> local_gen.fcl
-
-if [ -f $MY_OUTPUT_FILE ]; then
-    echo "File $MY_OUTPUT_FILE exists, so not generating again."
-else
-    echo "File $MY_OUTOUT_FILE does not exist, so running generation of it..."
-    echo "Running command 'lar -c local_gen.fcl -T ./genfile_hist.root -o $MY_OUTPUT_FILE -n $MY_N_EVENTS > $MY_OUT_LOG 2> $MY_ERR_LOG' "
-    lar -c local_gen.fcl -T ./genfile_hist.root -o $MY_OUTPUT_FILE -n $MY_N_EVENTS
-fi
+#!/bin/sh
+init_gen_common.sh prodgenie_common_uboone_NCDeltaRadFiltered.fcl 250000
