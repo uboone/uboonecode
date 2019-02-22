c8b2e07389493792c555f27e247f3b83caadbcc2
Wesley Ketchum <wketchum@fnal.gov>
2019-02-22 14:53:36 -0600

    add CCPi0 filtering script, and add better provenance tracking for a production version of SimInfoMixer

--- tools/overlay_scripts/init_gen_common.sh
+++ tools/overlay_scripts/init_gen_common.sh
@@ -10,6 +10,11 @@ echo "#include \"$MY_FCL_FILE\"" > local_gen.fcl
 echo "physics.producers.generator.FluxCopyMethod: \"IFDH\"" >> local_gen.fcl
 echo "physics.producers.generator.MaxFluxFileMB: 500" >> local_gen.fcl
 
+echo "#include \"$MY_FCL_FILE\"" > local_gen_include.fcl
+echo "physics.producers.generator.FluxCopyMethod: \"IFDH\"" >> local_gen_include.fcl
+echo "physics.producers.generator.MaxFluxFileMB: 500" >> local_gen_include.fcl
+echo "gen_detail: { physics: {@table::physics} services: {@table::services} outputs: {@table::outputs} source: {@table::source} process_name: @local::process_name }" >> local_gen_include.fcl
+
 if [ -f $MY_OUTPUT_FILE ]; then
     echo "File $MY_OUTPUT_FILE exists, so not generating again."
 else
