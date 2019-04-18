b54c82d7a18baccf448416b8b26248019afa211d
José I. Crespo-Anadón <jcrespo@nevis.columbia.edu>
2019-04-18 14:43:35 -0500

    Add script used to make the plots shown at the APE Meeting on April 16th, 2019

---
+++ uboone/SNAnalysis/plot.py
@@ -0,0 +1,163 @@
+from ROOT import TFile, TH1F, TCanvas, gDirectory, gROOT, gPad, TLegend, TPad, TLine
+import math
+
+gROOT.ForceStyle()
+
+# ROOT files with histograms
+snfile = TFile("/home/jcrespo/MicroBooNE/SNAnalysis/19021/spectra/SNRun19021_960files3planes_SNMichelAna_hist_v2.root")
+extunbfile = TFile("/home/jcrespo/MicroBooNE/SNAnalysis/EXTUNB/spectra/Run3_25kfiles3planes_SNMichelAna_hist_v2.root")
+
+# Name of histograms to retrieve
+hlistname = [ "heSpectrum",
+          "hgSpectrum",
+          "htotSpectrum",
+          "heHitMult",
+          "hgHitMult",
+          "htotHitMult",
+          "heHitSpectrum",
+          "hgHitSpectrum",
+          "htotHitSpectrum",
+          "hEventHitMult",
+          "hEventHitSpectrum" ]
+
+# Fraction of canvas for the bottom pad (make it 0 if you do not want one)
+frontierpad = 0.3
+logx = True
+
+# Lists to hold histograms retrieved from files
+snhlist= []
+extunbhlist = []
+
+# Normalization factor for the 3 TPC planes
+normfactor = [0., 0., 0.]
+
+# Retrieve objects from SN stream file
+for rootdir in snfile.GetListOfKeys():
+    snfile.cd( rootdir.GetName() )
+    for hname in hlistname:
+        htemp = TH1F()
+        gDirectory.GetObject( hname, htemp )
+        htemp.SetNameTitle( "sn_" + htemp.GetName() + "_" + rootdir.GetName()[-1], htemp.GetTitle() + " plane " + rootdir.GetName()[-1] )
+        htemp.Sumw2()
+        snhlist.append( htemp )
+        # Get normalization factor numerator
+        if htemp.GetName().startswith("sn_hEventHitMult"):
+            # print "%s %i " % (htemp.GetName(), htemp.GetEntries())
+            normfactor[int( rootdir.GetName()[-1] )] = htemp.GetEntries()
+
+# Retrieve objects from EXT unbiased file
+for rootdir in extunbfile.GetListOfKeys():
+    extunbfile.cd( rootdir.GetName() )
+    for hname in hlistname:
+        htemp = TH1F()
+        gDirectory.GetObject( hname, htemp )
+        # Get exposure (number of events) from hEventHitMult
+        htemp.SetNameTitle( "extunb_" + htemp.GetName() + "_" + rootdir.GetName()[-1], htemp.GetTitle() + " plane " + rootdir.GetName()[-1] )
+        # Get normalization factor denominator
+        if htemp.GetName().startswith("extunb_hEventHitMult"):
+            # print "%s %i " % (htemp.GetName(), 2.*htemp.GetEntries())
+            normfactor[int( rootdir.GetName()[-1] )] *= 1./(2.*htemp.GetEntries())
+            # For hit multiplicity/event we have to correct for the different event size
+            hnew = htemp.Clone()
+            hnew.Reset()
+            for ibin in xrange( htemp.GetNbinsX() ):
+                # Trigger stream events are twice as long as SN stream ones, so they can have twice as many hits
+                ibinnew = int( math.floor(ibin/2.) )
+                hnew.SetBinContent( ibinnew, hnew.GetBinContent(ibinnew) + htemp.GetBinContent(ibin) )
+            htemp = hnew    
+        htemp.Sumw2()
+        extunbhlist.append( htemp )
+
+# Lists to collect objects going out of scope
+clist = []
+extunbh_errorslist = []
+leglist = []
+hratiolist = []
+line1list = []
+
+for snh in snhlist:
+
+    c = TCanvas( "c_" + snh.GetName(), snh.GetTitle(), 800 if frontierpad else 1600, 800 )
+
+    if frontierpad:
+        ### Histograms plot ###
+        pad1 = TPad( "pad1_" + snh.GetName(), snh.GetTitle(), 0., frontierpad, 1., 1. )
+        pad1.SetBottomMargin(0) # Join with lower plot
+        pad1.Draw()
+        pad1.cd()
+
+    extunbh = extunbhlist[snhlist.index(snh)]
+    extunbh_integral = extunbh.Integral()
+    # Apply normalization factor based on last character of name
+    if extunbh.GetName().startswith("extunb_hEventHitMult"):
+        # For the event hit multiplicity the event size is corrected differently
+        extunbh.Scale( 2.*normfactor[int( snh.GetName()[-1] )] )
+    else:
+        extunbh.Scale( normfactor[int( snh.GetName()[-1] )] )
+    extunbh.SetMarkerColor(2)
+    extunbh.SetLineColor(2)
+    extunbh_errors = extunbh.Clone( extunbh.GetName() + "_errors" )
+    #extunbh_errors.SetFillColor(46)
+    extunbh_errors.SetFillColor(46)
+    extunbh_errors.Draw("E2") # Rectangle error boxes in the background
+    #extunbh_errors.DrawNormalized("E2") # Rectangle error boxes in the background
+    extunbh_errors.GetYaxis().SetRangeUser( 0.001, 
+        1.1*extunbh_errors.GetMaximum() if ( extunbh_errors.GetMaximum() > snh.GetMaximum() ) else 1.1*snh.GetMaximum()  )
+    #extunbh_errors.GetYaxis().SetLabelSize( (1. - frontierpad)*extunbh_errors.GetYaxis().GetLabelSize() )
+    extunbh.Draw("hist same") # Central value line
+    #extunbh.DrawNormalized("hist same") # Central value line
+    snh.SetMarkerStyle(21)
+    snh.SetMarkerSize(0.5)
+    snh.Draw("E P0 X0 same") # SN data
+    #snh.DrawNormalized("E P0 X0 same") # SN data
+    leg = TLegend(0.6, 0.7, 1.0, 1.0)
+    leg.AddEntry( extunbh_errors, "Trigger stream (%i entries)" % int( extunbh_integral ), "FLP" )
+    leg.AddEntry( snh, "SN stream (%i entries)" % int( snh.Integral() ), "LEP" )
+    if logx: leg.SetFillStyle(0) # Make it transparent to mitigate the change of the distribution
+    leg.Draw()
+    if logx: gPad.SetLogx()
+    gPad.Modified()
+    gPad.Update()
+
+    ### Ratio plot ###
+    if frontierpad:
+        c.cd()
+        pad2 = TPad( "pad2_" + snh.GetName(), snh.GetTitle(), 0., 0.05, 1., frontierpad )
+        pad2.SetTopMargin(0) # Join with upper plot
+        pad2.SetBottomMargin(0.3)
+        pad2.Draw()
+        pad2.cd()
+
+        hratio = snh.Clone()
+        hratio.Sumw2()
+        hratio.Divide(extunbh)
+            
+        hratio.GetYaxis().SetTitle("SN/Trigger")
+        hratio.GetYaxis().SetRangeUser(0., 1.999)
+        hratio.GetYaxis().SetNdivisions(505)
+        hratio.GetYaxis().SetLabelSize( ((1. - frontierpad)/frontierpad)*extunbh_errors.GetYaxis().GetLabelSize() )
+        hratio.GetYaxis().SetTitleSize( ((1. - frontierpad)/frontierpad)*extunbh_errors.GetYaxis().GetTitleSize() )
+        hratio.GetYaxis().SetTitleOffset( frontierpad/(1. - frontierpad)*extunbh_errors.GetYaxis().GetTitleOffset() )
+        hratio.GetYaxis().CenterTitle()
+        hratio.GetXaxis().SetLabelSize( ((1. - frontierpad)/frontierpad)*extunbh_errors.GetXaxis().GetLabelSize() )
+        hratio.GetXaxis().SetTitleSize( ((1. - frontierpad)/frontierpad)*extunbh_errors.GetXaxis().GetTitleSize() )
+        hratio.SetMarkerStyle(21)
+        hratio.SetMarkerSize(0.5)
+        hratio.Draw("E P0 X0")
+        line1 = TLine( extunbh_errors.GetXaxis().GetXmin(), 1., extunbh_errors.GetXaxis().GetXmax(), 1. )
+        line1.SetLineWidth(1)
+        line1.SetLineColor(2)
+        line1.Draw()
+        hratio.Draw("E P0 X0 same") # Draw again over line
+        if logx: gPad.SetLogx()        
+        # Keep the objects in memory
+        line1list.append(line1)
+        hratiolist.append(hratio)
+
+    leglist.append(leg)
+    extunbh_errorslist.append(extunbh_errors)
+    c.Print( c.GetName() + ("_logx" if logx else "") + ("_ratio.png" if frontierpad else ".png") )
+    #c.Print( "norm_" + c.GetName() + ".png" )
+    clist.append(c)
+
+raw_input("Press the <ENTER> key to continue...")
