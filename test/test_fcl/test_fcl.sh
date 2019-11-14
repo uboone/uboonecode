f3a9fedf1bd39d45aafc75382f2017f5c5571fe8
Herbert Greenlee <greenlee@fnal.gov>
2019-11-14 11:11:02 -0600

    Add test for development conditions database.

--- test/test_fcl/test_fcl.sh
+++ test/test_fcl/test_fcl.sh
@@ -43,4 +43,11 @@ do
     exit 1
   fi
   
+  # Check for connections to development conditions database.
+
+  if egrep -q uboonecon_dev $fclout; then
+    echo "Found connection to development conditions database."
+    exit 1
+  fi
+
 done
