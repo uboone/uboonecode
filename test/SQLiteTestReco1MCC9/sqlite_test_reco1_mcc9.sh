98d13bbfb056129ef08a547f5085c22a32c97a2e
Herbert Greenlee <greenlee@fnal.gov>
2020-04-10 10:33:17 -0500

    Add sqlite tests with TestMode enabled.

---
+++ test/SQLiteTestReco1MCC9/sqlite_test_reco1_mcc9.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+# Save a copy of the environment (for debugging).
+
+env > env.txt
+
+# Exit if stash cache isn't mounted.
+
+UBOONE_EXAMPLE_DATA_DIR=/cvmfs/uboone.osgstorage.org/stash/uboone_example_data
+if [ ! -d $UBOONE_EXAMPLE_DATA_DIR ]; then
+  echo "Quittig because stash cache isn't available."
+  exit
+fi
+
+# Set wire cell path.
+
+export WIRECELL_PATH=${UBOONEDATA_DIR}/WireCellData:${WIRECELL_FQ_DIR}/share/wirecell
+
+# Set up python path.
+
+export PYTHONPATH=$UBUTIL_DIR/python:$PYTHONPATH
+
+# Set experiment environment variables (not set by mrbsetenv, but needed by IFDH).
+
+export EXPERIMENT=uboone
+export SAM_EXPERIMENT=uboone
+
+# This script runs the full mc+reco chain using standard released fcl files.
+
+input=$UBOONE_EXAMPLE_DATA_DIR/swizzled/PhysicsRun-2019_5_9_15_17_29-0022417-00198_20190620T162349_bnb_2_20190620122031_merged.root
+fcl=test_sqlite_reco1_mcc9.fcl
+output=`basename $fcl .fcl`.root
+out=`basename $fcl .fcl`.out
+err=`basename $fcl .fcl`.err
+cmd="lar --rethrow-all -c $fcl -s $input -o $output -n 5"
+echo $cmd
+$cmd > $out 2> $err
+stat=$?
+echo "Command finished with status $stat"
+if [ $stat -ne 0 ]; then
+  exit $stat
+fi
+
+# Done (success).
+
+for root in *.root
+do
+  rootstat.py $root > ${root}.rootstat
+  rm $root   # Clean up.
+done
