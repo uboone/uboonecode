9384c01f545c1226e4a8da17b00b574dfb682a48
cthorpe123 <c.thorpe@lancaster.ac.uk>
2021-11-11 03:23:06 -0600

    Added hyperon and neutron filters

---
+++ tools/overlay_scripts/init_gen_numi_rhc_hyperonfilter.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+init_gen_common.sh prodgenie_numi_nu_uboone_rhc.fcl 5000
+lar -c wirecell_g4_uboone.fcl -s genfile.root.local -o genfile_g4.root.local -n -1
+lar -c run_HyperonFilter.fcl -s genfile_g4.root.local -o genfile_filtered.root.local -n -1
+mv genfile_filtered.root.local genfile.root.local 
+rm genfile_g4.root.local
