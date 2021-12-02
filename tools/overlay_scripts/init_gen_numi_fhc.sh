3a86e042c1538301a0cd82df32bf672339aa72c1
cthorpe123 <c.thorpe@lancaster.ac.uk>
2021-12-02 08:10:23 -0600

    Improving neutron filters

---
+++ tools/overlay_scripts/init_gen_numi_fhc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+init_gen_common.sh prodgenie_numi_dirt_nu_wirecell_g4_uboone_fhc.fcl 6000
--- tools/overlay_scripts/init_gen_numi_fhc_nuwro_neutronfilter.sh
+++ tools/overlay_scripts/init_gen_numi_fhc_nuwro_neutronfilter.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-init_gen_common.sh prodgenie_numi_nu_uboone_fhc.fcl 6000
+source nuwro_script.sh
+init_gen_common.sh prodhepmc_uboone_10000.fcl  10000
 lar -c run_PrimaryNeutronFilter.fcl -s genfile.root.local -o genfile_gen_filtered.root.local -n -1
 rm genfile.root.local
 lar -c wirecell_g4_uboone.fcl -s genfile_gen_filtered.root.local -o genfile_g4.root.local -n -1
