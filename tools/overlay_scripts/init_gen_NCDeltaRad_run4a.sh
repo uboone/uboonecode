be4442cdd9b230b431343e0c7dcd0fbf60ea8cee
Herbert Greenlee <greenlee@fnal.gov>
2023-10-09 21:56:39 -0500

    Add run 4a init_gen scripts.

---
+++ tools/overlay_scripts/init_gen_NCDeltaRad_run4a.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+init_gen_common.sh prodgenie_common_uboone_NCDeltaRadFiltered_run4a.fcl 250000
---
+++ tools/overlay_scripts/init_gen_NCNoPi_run4a.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+init_gen_common.sh prodgenie_bnb_nu_filtered_NCNoPi_uboone_run4a.fcl 2000
